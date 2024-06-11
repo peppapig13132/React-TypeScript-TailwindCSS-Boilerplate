dac01cf22de686679b67971d6525c39487d67e6c
peppapig13132 <peppapig13132@outlook.com>
2024-06-11 23:58:02 +0200

    Add footer

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,10 @@ function App() {
       <h2 className="text-4xl my-3 text-slate-700">
         Hello React.js!
       </h2>
-      <p className="my-3 text-slate-700">
+      <p className="text-slate-700">
         React.js boilerplate pre-configured with TypeScript and Tailwind CSS
       </p>
+      <p className="mt-12 text-slate-700">Made with ❤️ by <a target="_blank" href="https://github.com/peppapig13132">Pepp</a></p>
     </div>
   );
 }
